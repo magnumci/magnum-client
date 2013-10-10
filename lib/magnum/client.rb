9076c582938bb37aa06b9c719baa41e0169cd978
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-10-10 18:16:52 -0500

    Add method to send payload to api

--- lib/magnum/client.rb
+++ lib/magnum/client.rb
@@ -20,5 +20,20 @@ module Magnum
       @api_key = api_key
       @debug = false
     end
+
+    # Send commit payload data to magnum
+    #
+    # @param [String] project API token
+    # @param [String] provider name (github, bitbucket, gitlab, etc)
+    # @param [Hash] payload data
+    #
+    def self.send_payload(project_token, provider, data)
+      response = Faraday.post("https://magnum-ci.com/api/v1/payload/#{provider}",
+        token: project_token,
+        payload: data
+      )
+
+      response.success?
+    end
   end
 end
