e0e67a5899fd0943c5b73f229ab85b48872df8f2
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-02-20 21:20:46 -0600

    Reformat profile endpoint code

--- lib/magnum/client/endpoints/profile.rb
+++ lib/magnum/client/endpoints/profile.rb
@@ -1,15 +1,11 @@
 class Magnum::Client
-  module Endpoints
-    module Profile
-      
-      def profile
-        get("profile")
-      end
+  module Endpoints::Profile
+    def profile
+      get("profile")
+    end
 
-      def update_profile(attributes={})
-        put("profile", user: attributes)
-      end
-
+    def update_profile(attributes={})
+      put("profile", user: attributes)
     end
   end
 end
