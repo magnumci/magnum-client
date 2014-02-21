eee979ce5cb80d196ec13f2a35b8833d24ad9cf7
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-02-20 21:22:00 -0600

    Reformat authentication endpoint code

--- lib/magnum/client/endpoints/authentication.rb
+++ lib/magnum/client/endpoints/authentication.rb
@@ -1,14 +1,7 @@
 class Magnum::Client
-  module Endpoints
-    module Authentication
-
-      def authenticate(email, password)
-        request(:post, "authenticate", {
-          email: email,
-          password: password
-        })
-      end
-      
+  module Endpoints::Authentication
+    def authenticate(email, password)
+      post("authenticate", email: email, password: password)
     end
   end
 end
